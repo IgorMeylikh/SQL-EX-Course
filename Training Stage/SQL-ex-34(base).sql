0f21241b2f122c14a224235b71293382be196f36
Мейлих Игорь Юрьевич <igor.meilih@osinit.com>
2021-12-15 09:11:23 +0300

    Добавил первый файл с номером 34 из уроков по SQL

---
+++ Training Stage/SQL-ex-34(base).sql
@@ -0,0 +1,7 @@
+-- По Вашингтонскому международному договору от начала 1922 г. запрещалось строить линейные корабли водоизмещением более 35 тыс.тонн. 
+-- Укажите корабли, нарушившие этот договор (учитывать только корабли c известным годом спуска на воду). 
+-- Вывести названия кораблей. 
+SELECT name 
+FROM Ships, Classes
+WHERE Classes.type = 'bb' AND Ships.launched >= 1922 AND Ships.launched IS NOT NULL AND Classes.displacement > 35000 AND Ships.class = Classes.class
+GROUP BY name
